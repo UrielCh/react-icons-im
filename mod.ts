c8e9eaec65c444c4ab9a00bbf6591687e5e4c462
urielch <uriel.chemouni@gmail.com>
2023-05-31 18:34:29 +0300

    bump version

--- mod.ts
+++ mod.ts
@@ -17,8 +17,8 @@
  *   "imports": {
  *     "preact":  "https://esm.sh/preact@10.15.1",
  *     "preact/": "https://esm.sh/preact@10.15.1/",
- *     "react-icons/im":  "https://cdn.jsdelivr.net/gh/urielch/react-icons-im@1.0.5/mod.ts",
- *     "react-icons/im/": "https://cdn.jsdelivr.net/gh/urielch/react-icons-im@1.0.5/ico/",
+ *     "react-icons/im":  "https://cdn.jsdelivr.net/gh/urielch/react-icons-im@1.0.6/mod.ts",
+ *     "react-icons/im/": "https://cdn.jsdelivr.net/gh/urielch/react-icons-im@1.0.6/ico/",
  *   }
  * }
  * ```
@@ -26,7 +26,7 @@
  * ## Direct import sample
  * 
  * ```ts
- * import { ImHome } from "https://deno.land/x/react_icons_im@1.0.5/mod.ts"
+ * import { ImHome } from "https://deno.land/x/react_icons_im@1.0.6/mod.ts"
  * ```
  * 
  * ## import_map import sample
