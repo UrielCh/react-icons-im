8b7ec2f7c0b8df20a6f75ebea661faccc3026342
urielch <uriel.chemouni@gmail.com>
2024-07-17 13:33:37 +0200

    order icons by name

--- ico/ImEmbed.ts
+++ ico/ImEmbed.ts
@@ -1,6 +1,6 @@
 import { GenIcon, type IconBaseProps } from "../deps.ts";
 
 export function ImEmbed(props: IconBaseProps) {
-  return GenIcon({tag:"svg",attr:{version:"1.1",viewBox:"0 0 16 16"},child:[{tag:"path",attr:{d:"M9 11.5l1.5 1.5 5-5-5-5-1.5 1.5 3.5 3.5z"}},{tag:"path",attr:{d:"M7 4.5l-1.5-1.5-5 5 5 5 1.5-1.5-3.5-3.5z"}}]})(props);
+  return GenIcon({tag:"svg",attr:{version:"1.1",viewBox:"0 0 16 16"},child:[{tag:"path",attr:{d:"M9 11.5l1.5 1.5 5-5-5-5-1.5 1.5 3.5 3.5z"}},{tag:"path",attr:{d:"M7 4.5l-1.5-1.5-5 5 5 5 1.5-1.5-3.5-3.5z"},child:[]}]})(props);
 }
 export default ImEmbed;
