3eecb4987adb9116cda1d142ff01b813c7d3ccc6
urielch <uriel.chemouni@gmail.com>
2024-07-18 10:53:28 +0200

    update doc

--- README.md
+++ README.md
@@ -8,7 +8,7 @@
 
 [See available icons here](https://react-icons.deno.dev/im)
 
-## install packahe
+## install the module
 
 ```bash
 deno add @preact-icons/im
@@ -17,36 +17,15 @@ pnpm dlx jsr add @preact-icons/im
 bunx jsr add @preact-icons/im
 ```
 
-You may need to update your preact mapping to:
- - "preact": "npm:preact@10.22.1",
- - "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
- - "preact/hooks": "npm:preact@10.22.1/hooks",
-
-
-# IcoMoon Free icons for preact
-
-[![JSR](https://jsr.io/badges/@preact-icons/im)](https://jsr.io/@preact-icons/im)
-
-**License** [CC BY 4.0 License](https://github.com/Keyamoon/IcoMoon-Free/blob/master/License.txt)
-
-**Project** [https://github.com/Keyamoon/IcoMoon-Free](https://github.com/Keyamoon/IcoMoon-Free)
-
-[See available icons here](https://react-icons.deno.dev/im)
-
-## install packahe
-
-```bash
-deno add @preact-icons/im
-dnpx jsr add @preact-icons/im
-pnpm dlx jsr add @preact-icons/im
-bunx jsr add @preact-icons/im
+You may need to update your preact mapping to avoid mixing JSR and http import:
+Currently Deno fresh import preact using https://esm.sh/preact http import can not be mixed with JSR package, so you may need to update your preact mapping:
+```json
+{
+ "preact": "npm:preact@10.22.1",
+ "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
+ "preact/hooks": "npm:preact@10.22.1/hooks",
+}
 ```
-
-You may need to update your preact mapping to:
- - "preact": "npm:preact@10.22.1",
- - "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
- - "preact/hooks": "npm:preact@10.22.1/hooks",
-
 
 ## import an icon from all icons
 
@@ -65,4 +44,3 @@ or using default export
 ```ts
 import ImHome from "react-icons/im/ImHome.ts"
 ```
-
--- mod.ts
+++ mod.ts
@@ -10,7 +10,7 @@
  * 
  * [See available icons here](https://react-icons.deno.dev/im)
  * 
- * ## install packahe
+ * ## install the module
  * 
  * ```bash
  * deno add @preact-icons/im
@@ -19,36 +19,15 @@
  * bunx jsr add @preact-icons/im
  * ```
  * 
- * You may need to update your preact mapping to:
- *  - "preact": "npm:preact@10.22.1",
- *  - "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
- *  - "preact/hooks": "npm:preact@10.22.1/hooks",
- * 
- * 
- * # IcoMoon Free icons for preact
- * 
- * [![JSR](https://jsr.io/badges/@preact-icons/im)](https://jsr.io/@preact-icons/im)
- * 
- * **License** [CC BY 4.0 License](https://github.com/Keyamoon/IcoMoon-Free/blob/master/License.txt)
- * 
- * **Project** [https://github.com/Keyamoon/IcoMoon-Free](https://github.com/Keyamoon/IcoMoon-Free)
- * 
- * [See available icons here](https://react-icons.deno.dev/im)
- * 
- * ## install packahe
- * 
- * ```bash
- * deno add @preact-icons/im
- * dnpx jsr add @preact-icons/im
- * pnpm dlx jsr add @preact-icons/im
- * bunx jsr add @preact-icons/im
+ * You may need to update your preact mapping to avoid mixing JSR and http import:
+ * Currently Deno fresh import preact using https://esm.sh/preact http import can not be mixed with JSR package, so you may need to update your preact mapping:
+ * ```json
+ * {
+ *  "preact": "npm:preact@10.22.1",
+ *  "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
+ *  "preact/hooks": "npm:preact@10.22.1/hooks",
+ * }
  * ```
- * 
- * You may need to update your preact mapping to:
- *  - "preact": "npm:preact@10.22.1",
- *  - "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
- *  - "preact/hooks": "npm:preact@10.22.1/hooks",
- * 
  * 
  * ## import an icon from all icons
  * 
@@ -67,8 +46,8 @@
  * ```ts
  * import ImHome from "react-icons/im/ImHome.ts"
  * ```
- * 
  * @module
+ * 
  */
 
 import { GenIcon } from "./deps.ts";
